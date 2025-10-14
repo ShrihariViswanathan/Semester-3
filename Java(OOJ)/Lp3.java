20521412315ecc685c546447c76f6066eb0a0698
ShrihariViswanathan <shrihariviswanathan.cs24@bmsce.ac.in>
2025-10-14 12:16:05 +0530

    Add files via upload

---
+++ Java(OOJ)/Lp3.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+class Book{
+    String name;
+    String author;
+    double price;
+    int numPages;
+
+    Book(String name, String author, double price, int numPages){
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.numPages = numPages;
+    }
+
+
+    public String toString(){
+        String name, author, price, numPages;
+        name = "Book name: " + this.name + "\n";
+        author = "Author name: " + this.author + "\n";
+        price = "Price: " + this.price + "\n";
+        numPages = "Number of pages: " + this.numPages + "\n";
+
+        return name + author + price + numPages;
+    }
+
+} //end of class
+
+
+class Lp3{
+    public static void main(String args[]){
+
+    Scanner s = new Scanner(System.in);
+
+    int n; 
+    String name; 
+    String author; 
+    double price; 
+    int numPages;
+
+    System.out.print("No.of Books: ");
+    n = s.nextInt();
+        s.nextLine();
+
+    Book b[];
+    b = new Book[n];
+
+    for (int i = 0; i < n; i++){
+         //name
+        
+         System.out.print("Name of Book " + (i+1));
+        name = s.nextLine();
+
+        System.out.print("Author of Book " + (i + 1));
+        author = s.nextLine();
+
+        System.out.print("Price of Book " + (i + 1));
+        price = s.nextDouble();
+
+        System.out.print("No.of Pages for Book " + (i + 1)) ;
+        numPages = s.nextInt();
+
+        b[i] = new Book(name, author, price, numPages);     
+    }
+
+
+    for (int i = 0; i < b.length; i++){
+        System.out.println(b[i]);
+    }
+
+    }
+}
+
+

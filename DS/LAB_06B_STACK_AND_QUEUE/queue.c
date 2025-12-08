2f44ab3da636fa2773e7cf910f6ed0343ef3933e
ShrihariViswanathan <shrihariviswanathan.cs24@bmsce.ac.in>
2025-12-08 10:17:11 +0530

    Add files via upload

---
+++ DS/LAB_06B_STACK_AND_QUEUE/queue.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node* front=NULL;
+struct node* rear=NULL;
+void enqueue(int w){
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=w;
+    newnode->next=NULL;
+    if(rear==NULL){
+        front=rear=newnode;
+        return;
+    }
+    rear->next=newnode;
+    rear=newnode;
+}
+void dequeue(){
+    if(front==NULL){
+        printf("QUEUE UNDERFLOW\n");
+        return;
+    }
+    struct node* temp=front;
+    front=front->next;
+    if(front==NULL){
+        rear=NULL;
+    }
+    printf("The dequeued element is: %d\n",temp->data);
+    free(temp);
+}
+void display(){
+    struct node* temp=front;
+    if(temp==NULL){
+        printf("QUEUE IS EMPTY\n");
+        return;
+    }
+    printf("The elements in the queue are:\n");
+    while(temp!=NULL){
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+}
+int main(){
+    int choice, value;
+    while(1){
+        printf("\nEnter the operation (1: Enqueue, 2: Dequeue, 3: Display, 4: Exit): ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter the value to enqueue: ");
+                scanf("%d",&value);
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("INVALID CHOICE\n");
+        }
+    }
+    return 0;
+}
---
+++ DS/LAB_06B_STACK_AND_QUEUE/stack lnklist.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node* top=NULL;
+void push(int w){
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=w;
+    newnode->next=top;
+    top=newnode;
+}
+void pop(){
+    if(top==NULL){
+        printf("STACK UNDERFLOW\n");
+        return;
+    }
+    struct node* temp=top;
+    top=top->next;  
+    printf("The popped element is: %d\n",temp->data);
+    free(temp);
+}
+void display(){
+    struct node* temp=top;
+    if(temp==NULL){
+        printf("STACK IS EMPTY\n");
+        return;
+    }
+    printf("The elements in the stack are:\n");
+    while(temp!=NULL){
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+}
+int main(){
+    int choice, value;
+    while(1){
+        printf("\nEnter the operation (1: Push, 2: Pop, 3: Display, 4: Exit): ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter the value to push: ");
+                scanf("%d",&value);
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("INVALID CHOICE\n");
+        }
+    }
+    return 0;
+}
